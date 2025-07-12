82dacc04fcfde98162422db03e6b146e9ab0d81f
ShuhamRaz <pankajkr6810@gmail.com>
2025-07-12 20:30:13 +0530

    EDA

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
 HYPEN_E_DOT = "-e ."
 
-def get_requirements(file_path:str):
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function will return the list of requirements
+    This function returns the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n"," ") for req in requirements]
-
+        requirements = [req.strip() for req in file_obj.readlines()]
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
@@ -23,6 +21,4 @@ setup(
     author_email="pankajkr6810@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt")
-
-
 )

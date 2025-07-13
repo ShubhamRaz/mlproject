ae046450df93f226c38a21f42fc259901086ac18
ShuhamRaz <pankajkr6810@gmail.com>
2025-07-13 15:17:06 +0530

    model trainer with hyperparameter tuning

--- src/components/data_injection.py
+++ src/components/data_injection.py
@@ -7,6 +7,9 @@ from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -48,5 +51,7 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,teat_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, teat_arr))
 
